38b5877d41d0775a74e9f395c140573c3676056a
Alfonso Nombela Moreno <alfon3193@anombela.local>
2015-05-08 14:39:29 +0200

    añadido nombre y contraseña

--- cms_users_put/models.py
+++ cms_users_put/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 
-# Create your models here.
 
+# users: anombela password: anombela
+
+# Create your models here.
 class Pages(models.Model):
     name = models.CharField(max_length=32)
     page = models.TextField()
